efdb5b0163015393d21d79fed8d709840db14f99
hrmoore927 <hrmoore927@gmail.com>
2017-11-02 14:28:01 -0400

    Finished tutorial 9

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -25,4 +25,13 @@ class ProductController extends Controller
         $request->session()->put('cart', $cart);
         return redirect()->route('product.index');
     }
+    
+    public function getCart(){
+        if (!Session::has('cart')){
+            return view('shop.shopping-cart');
+        }
+        $oldCart = Session::get('cart');
+        $cart = new Cart($oldCart);
+        return view('shop.shopping-cart', ['products' => $cart->items, 'totalPrice' => $cart->totalPrice]);
+    }
 }
